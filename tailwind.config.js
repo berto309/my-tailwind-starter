c04c32a6f051cecd55a7a46dfb69f98480b2336d
Robert Ntim <ntimrobert309@gmail.com>
2021-11-21 19:54:20 +0000

    my tailwind starter

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    "./web-ui/**/*.html"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+
+
+    extend: {
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+
+  ],
+}
